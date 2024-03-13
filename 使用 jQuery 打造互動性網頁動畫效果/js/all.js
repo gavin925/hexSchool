83e4039dd149855a31e9247cacf872b5bd1aebee
俊穎 巫 <gavin925@gmail.com>
2024-03-13 19:16:22 +0800

    update
    new&update

---
+++ 使用 jQuery 打造互動性網頁動畫效果/js/all.js
@@ -0,0 +1,43 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+$('.dropdown').click(function(event){
+  event.preventDefault();
+  $('.dropdown').toggleClass('active');
+  $('.dropdown-open').slideToggle();
+});
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal' ,
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+lightbox.option({
+      'resizeDuration': 200,
+      'wrapAround': true
+    });
+function showBtnCondition() {
+        if ($(this).scrollTop() > 300) {
+        $('#gotop').fadeIn();
+          } else {
+           $('#gotop').fadeOut();
+         }
+       }
+   $(window).scroll(showBtnCondition);
+  });
